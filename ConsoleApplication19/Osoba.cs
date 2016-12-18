c3b054530a2c4c53b19d23cfdcc2e4af9d2b2ecb
Piotrekk85 <piotr.jastr@gmail.com>
2016-12-18 15:11:22 +0100

    Add files via upload

---
+++ ConsoleApplication19/Osoba.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Osoba : IComparable<Osoba>
+    {
+        private int wiek;
+
+        public int Wiek
+        {
+            get { return wiek; }
+            set { wiek = value; }
+        }
+
+        private string imie;
+
+        public string Imie
+        {
+            get { return imie; }
+            set { imie = value; }
+        }
+
+        public Osoba(string imie, int wiek)
+        {
+            this.Wiek = wiek;
+            this.Imie = imie;
+        }
+
+        public override string ToString()
+        {
+            return Imie + ", " + Wiek;
+        }
+
+        public int CompareTo(Osoba innaOsoba)
+        {
+            if (this.Imie == innaOsoba.Imie)
+                return this.Wiek.CompareTo(innaOsoba.Wiek);
+            return this.Imie.CompareTo(innaOsoba.Imie);
+        }
+
+
+
+    }
+}
---
+++ ConsoleApplication19/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Osoba> osoby = new List<Osoba>();
+            osoby.Add(new Osoba("Jan", 34));
+            osoby.Add(new Osoba("Antoni", 24));
+            osoby.Add(new Osoba("Sylwia", 21));
+            osoby.Add(new Osoba("Agnieszka", 21));
+            osoby.Add(new Osoba("Tomasz", 52));
+            var wysz = osoby.FindAll(x => x.Wiek == 21);
+            foreach(var element in wysz)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
